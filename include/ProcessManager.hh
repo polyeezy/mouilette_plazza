014715dd41186ebcb456ce0baae192c2146ca076
weinha_l <loic.weinhard@epitech.eu>
2016-04-23 16:58:47 +0200

    [PROCESS MANAGER] - Ajout d'un ServerSocket dans ProcessManager

--- include/ProcessManager.hh
+++ include/ProcessManager.hh
@@ -5,7 +5,7 @@
 // Login   <weinha_l@epitech.net>
 //
 // Started on  Thu Apr 14 15:48:23 2016 Loïc Weinhard
-// Last update Fri Apr 22 17:02:48 2016 Valerian Polizzi
+// Last update Sat Apr 23 16:25:08 2016 Loïc Weinhard
 //
 
 #ifndef PROCESS_MANAGER_HH_
@@ -14,6 +14,7 @@
 # include <Parser.hh>
 # include <Process.hh>
 # include <Command.hh>
+# include <SocketServer.hh>
 
 class	ProcessManager
 {
@@ -21,6 +22,7 @@ private:
   int				_nb_threads;
   std::vector<Process*>		_processes;
   std::vector<Command*>		_orders;
+  SocketServer			_server;
 
 public:
   ProcessManager(const int);
